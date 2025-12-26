cb862c1bee9dc97391e9386b33b298f010b9c249
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-12-26 18:47:19 +0530

    Fix: Fixing the validation of casts and trailerUrl

--- src/middlewares/movie.middleware.js
+++ src/middlewares/movie.middleware.js
@@ -1,24 +1,26 @@
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../utils/common");
 
-async function validateMovieCreate(req, res) {
+async function validateMovieCreate(req, res, next) {
     // validate the name...
     if(!req.body.name){
         BadRequestError.error = "The name of the movie is not present in the request";
         return res.status(StatusCodes.BAD_REQUEST).json(BadRequestError);
     }
-    // validate the description...
+    // // validate the description...
     if(!req.body.description){
         BadRequestError.error = "The description of the movie is not present in the request";
         return res.status(StatusCodes.BAD_REQUEST).json(BadRequestError);
     }
-    // validate the description...
-    if(!req.body.casts || !(req.body.casts instanceof Array) || req.body.casts <= 0){
-        BadRequestError.error = "The description of the movie is not present in the request";
+    // validate the casts...
+    if(!req.body.casts || !(req.body.casts instanceof Array) || req.body.casts.length <= 0){
+        console.log(req.body.casts instanceof Array);
+        console.log(req.body.casts.length)
+        BadRequestError.error = "The casts of the movie is not present in the request";
         return res.status(StatusCodes.BAD_REQUEST).json(BadRequestError);
     }
     // validate the trailerUrl...
-    if(!req.body.trailerUrl || !(req.body.trailerUrl instanceof Array) || req.body.trailerUrl <= 0){
+    if(!req.body.trailerUrl || !(req.body.trailerUrl instanceof Array) || req.body.trailerUrl.length <= 0){
         BadRequestError.error = "The trailerUrl of the movie is not present in the request";
         return res.status(StatusCodes.BAD_REQUEST).json(BadRequestError);
     }
