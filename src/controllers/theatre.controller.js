a9773562b911c32eb02a7f8d5b0edd2f76af2996
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-12-30 04:22:32 +0530

    feat: Creating the api to add the movies in particular theatre

--- src/controllers/theatre.controller.js
+++ src/controllers/theatre.controller.js
@@ -39,7 +39,30 @@ async function getTheatreById_controller(req,res){
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
     }
 }
+
+async function updateMovieInTheatre_controller(req, res) {
+    try {
+        const response = await theatreService.updateMovieInTheatre(
+            req.params.id,
+            req.body.movieIds,
+            req.body.insert
+        );
+        if(response.err){
+            ErrorResponse.error = response.err;
+            return res.status(response.code).json(ErrorResponse);
+        }
+        SuccessResponse.data = response;
+        if(req.body.insert) SuccessResponse.message = "SuccessFully Adding the Movie in Theatre !!"
+        else SuccessResponse.message = "SuccessFully Removing the Movie from Theatre !!"
+        return res.status(StatusCodes.OK).json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        ErrorResponse.message = error.message;
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+    }
+}
 module.exports = {
     createTheare_controller,
-    getTheatreById_controller
+    getTheatreById_controller,
+    updateMovieInTheatre_controller
 }
--- src/routers/v1/theatre.router.js
+++ src/routers/v1/theatre.router.js
@@ -7,11 +7,16 @@ const theatreRouter = express.Router();
 /**
  * http://localhost:9999/mba/api/v1/theatre/
 */
-theatreRouter.post('/', TheatreMiddleware, theatreController.createTheare_controller);
+theatreRouter.post('/', TheatreMiddleware.validateTheatreCreate, theatreController.createTheare_controller);
 
 /**
  * http://localhost:9999/mba/api/v1/theatre/:id --> (id is urlParams)
 */
 theatreRouter.get('/:id',theatreController.getTheatreById_controller);
+
+/**
+ * http://localhost:9999/mba/api/v1/theatre/:id/movies
+*/
+theatreRouter.patch('/:id/movies', TheatreMiddleware.validateUpdateMovieInTheatreParams, theatreController.updateMovieInTheatre_controller);
 
 module.exports = theatreRouter;
