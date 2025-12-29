074768c128607dde4dc9b23d5d7ccd220e84cc3f
Saniketbaisoya <sanibaisoya001@gmail.com>
2025-12-29 17:18:47 +0530

    Feat: Associating the theatre to Movie model

--- src/models/theatre.model.js
+++ src/models/theatre.model.js
@@ -17,7 +17,17 @@ const theatreSchema = new mongoose.Schema({
         type: Number,
         required: true,
     },
-    address: String
+    address: String,
+    /**
+     * Now yha hmne kri hai manual referencing, mtlb hmm yha dusre model -> Movie uski id's ko store krege
+     * Also yha hmm movies mai sirf ek movieId ko store nhi krege, multiple id's ko store krege 
+     * Because the theatre has many movies, isliye yha ref: 'Movie' yani ref mai voh model define kiya jiski id's store hogi
+     * And type ko ek array based bnaya, phele [ ] diya then usme type define kiya taki jo field hai voh ek aaray ki trh us type of data ko store kr ske....
+    */
+    movies: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Movie'
+    }
 
 },{timestamps: true});
 
