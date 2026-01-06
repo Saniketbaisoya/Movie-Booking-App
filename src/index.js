8e7949f9837b51cddc4c2b4dc7bd29dff5141c4a
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-01-06 17:07:41 +0530

    adding mongoose.debug func to get the info about the query execution

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const { PORT } = require('./configuration/serverConfig');
 const connectDb = require('./configuration/dbConfig');
 const { router } = require('./routers/routes');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 // parsing the req.body data through our main app server.....
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+
+mongoose.set('debug', true);
 
 // http://localhost:9999/mba/api
 app.use('/mba/api',router); // invoking the router in app.
