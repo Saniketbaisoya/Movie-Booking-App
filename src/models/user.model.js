16587f1766fe1c52a53a2111cedbd57e7ceac4c8
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-01-14 13:36:02 +0530

    feat: Creating the userSchema for auth

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+const { default: mongoose } = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        minLength: 6,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/Mw+([\-]?\w+)*@\w+([\-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email"],
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    userRole: {
+        type: String,
+        required: true,
+        default: 'CUSTOMER'
+    },
+    userStatus: {
+        type: String,
+        required: true,
+        default: 'APPROVED'
+    }
+
+},{timestamps: true});
+
+const User = mongoose.model('User',userSchema);
+module.exports =  User;
