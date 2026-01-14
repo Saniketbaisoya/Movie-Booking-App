19442c8abf07f0fcb1e6d25d828d9c8dcb642c3d
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-01-14 16:10:01 +0530

    feat: Adding the bcrypt for encryption of user password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
@@ -13,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        match: [/Mw+([\-]?\w+)*@\w+([\-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email"],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email"],
         lowercase: true,
         trim: true
     },
@@ -34,6 +35,12 @@ const userSchema = new mongoose.Schema({
     }
 
 },{timestamps: true});
+
+userSchema.pre('save', async function (next) {
+    const hashPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashPassword;
+    next();
+})
 
 const User = mongoose.model('User',userSchema);
 module.exports =  User;
