0af9b1fd155b79add51ab7b2f0b52b01cb0d6394
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-02-02 16:33:26 +0530

    adding streak

--- src/configuration/dbConfig.js
+++ src/configuration/dbConfig.js
@@ -5,7 +5,7 @@ const { MONGODB_URL } = require("./serverConfig");
 async function connectDb(){
     try {
         await mongoose.connect(MONGODB_URL);
-        console.log('SuccessFully Connected to the db server...')
+        console.log('SuccessFully Connected to the db server...');
     } catch (error) {
         console.log('Failed to connect to MongoDb...');
         console.log(error);
