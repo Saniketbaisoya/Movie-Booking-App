48366e716a9a0fc2c44f2e436fc7196fd85bf51c
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-02-03 15:47:57 +0530

    feat: Adding payment model

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,15 @@ const BOOKING_STATUS = {
     in_process : 'IN_PROCESS',
     cancelled : 'CANCELLED',
     successfull : 'SUCCESSFULL'
-}
+};
+
+const PAYMENT_STATUS = {
+    pending: 'PENDING',
+    failed: 'FAILED',
+    success: 'SUCCESS'
+};
 
 module.exports = {
-    BOOKING_STATUS
+    BOOKING_STATUS,
+    PAYMENT_STATUS
 }
